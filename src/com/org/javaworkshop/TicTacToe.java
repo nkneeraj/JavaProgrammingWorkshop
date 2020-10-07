49704784501c30bc758d47e3d6c26ecc6782ebd9
nkneeraj <nkneerj641@gmail.com>
2020-10-07 09:12:32 +0530

    [Neeraj]Added UC1: Create gameBoard.

--- src/com/org/javaworkshop/TicTacToe.java
+++ src/com/org/javaworkshop/TicTacToe.java
@@ -2,7 +2,18 @@ package com.org.javaworkshop;
 
 public class TicTacToe {
 
+	//master
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
+		char[] gameBoard = createBoard();
+	}
+
+	//UC1 
+	public static char[] createBoard() {
+		char[] gameBoard = new char[10];
+		for (int i = 0; i < 10; i++) {
+			gameBoard[i] = ' ';
+		}
+		return gameBoard;
 	}
 }
