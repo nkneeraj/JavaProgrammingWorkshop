d74bf5c91b47b11a7f955bbcd5cd451f9db0d04c
nkneeraj <nkneerj641@gmail.com>
2020-10-07 10:14:50 +0530

    [Neeraj]Added UC3: Show gameBoard.

--- src/com/org/javaworkshop/TicTacToe.java
+++ src/com/org/javaworkshop/TicTacToe.java
@@ -7,17 +7,18 @@ public class TicTacToe {
 	private static Scanner sc = new Scanner(System.in);
 	private static char playerMark;
 	private static char computerMark;
+	private static char[] gameBoard;
 	
 	//master
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
-		char[] gameBoard = createBoard();
-		
+		gameBoard = createBoard();
 		System.out.println("Enter player Mark");
 		playerMark = playerMark();
 		computerMark = (playerMark== 'X')? 'O':'X';
 		System.out.println("Player Mark: "+ playerMark);
 		System.out.println("Computer mark: "+ computerMark);
+		showBoard(gameBoard);
 	}
 
 	//UC1 
@@ -29,11 +30,22 @@ public class TicTacToe {
 		return gameBoard;
 	}
 	
+	//UC2
 	public static char playerMark()
 	{
 		System.out.println("Choose player option from 'X' & 'O'");
 		String player = sc.next();
 		char playerMark = player.charAt(0);
 		return playerMark;
+	}
+	
+	//UC3
+	public static void showBoard(char[] gameBoard)
+	{
+		System.out.println("ShowBoard");
+		for(int i =1;i<=(gameBoard.length)/3;i++)
+		{
+			System.out.println("Line: " +i+ "|" +gameBoard[i]+ gameBoard[i+1] + gameBoard[i+2]+ "|");
+		}
 	}
 }
