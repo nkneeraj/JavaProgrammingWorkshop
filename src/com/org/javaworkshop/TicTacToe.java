b125aa65421125187029f272044a593e189e84ea
nkneeraj <nkneerj641@gmail.com>
2020-10-07 11:53:26 +0530

    [Neeraj]Added: Valid user move.

--- src/com/org/javaworkshop/TicTacToe.java
+++ src/com/org/javaworkshop/TicTacToe.java
@@ -8,20 +8,24 @@ public class TicTacToe {
 	private static char playerMark;
 	private static char computerMark;
 	private static char[] gameBoard;
-	
-	//master
+
+	// master
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
 		gameBoard = createBoard();
 		System.out.println("Enter player Mark");
 		playerMark = playerMark();
-		computerMark = (playerMark== 'X')? 'O':'X';
-		System.out.println("Player Mark: "+ playerMark);
-		System.out.println("Computer mark: "+ computerMark);
+		computerMark = (playerMark == 'X') ? 'O' : 'X';
+		System.out.println("Player Mark: " + playerMark);
+		System.out.println("Computer mark: " + computerMark);
 		showBoard(gameBoard);
+		int position;
+		System.out.println("Enter the pos for user move");
+		position = sc.nextInt();
+		userMove(position);
 	}
 
-	//UC1 
+	// UC1
 	public static char[] createBoard() {
 		char[] gameBoard = new char[10];
 		for (int i = 0; i < 10; i++) {
@@ -29,23 +33,40 @@ public class TicTacToe {
 		}
 		return gameBoard;
 	}
-	
-	//UC2
-	public static char playerMark()
-	{
+
+	// UC2
+	public static char playerMark() {
 		System.out.println("Choose player option from 'X' & 'O'");
 		String player = sc.next();
 		char playerMark = player.charAt(0);
 		return playerMark;
 	}
-	
-	//UC3
-	public static void showBoard(char[] gameBoard)
-	{
+
+	// UC3
+	public static void showBoard(char[] gameBoard) {
 		System.out.println("ShowBoard");
-		for(int i =0;i<(gameBoard.length)/3;i++)
-		{
-			System.out.println("Line:" +(i+1)+ ": "  +gameBoard[i*3]+ "|" + gameBoard[3*i+1] + "|" + gameBoard[3*i+2]);
+		for (int i = 0; i < (gameBoard.length) / 3; i++) {
+			System.out.println("Line:" + (i + 1) + ": " + gameBoard[i * 3+1] + "|" + gameBoard[3 * i + 2] + "|"
+					+ gameBoard[3 * i + 3]);
 		}
 	}
+
+	public static int userMove(int position) {
+		
+		if(position <1 && position >9 && gameBoard[position]!=' ' 
+				&&gameBoard[position]!=computerMark)
+		{
+			System.out.println("Invalid position, enter again ");
+			position = sc.nextInt();
+			userMove(position);
+		}
+		else
+		{
+			gameBoard[position]= playerMark;
+			System.out.println("valid user position");
+			showBoard(gameBoard);
+		}
+		return position;
+	}
+
 }
