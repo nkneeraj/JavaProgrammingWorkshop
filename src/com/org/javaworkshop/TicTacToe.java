dd56841085942cdc97b32769e56f1def8a04ff71
nkneeraj <nkneerj641@gmail.com>
2020-10-07 13:01:08 +0530

    [Neeraj]Added: Toss functionality for Player and Computer.

--- src/com/org/javaworkshop/TicTacToe.java
+++ src/com/org/javaworkshop/TicTacToe.java
@@ -13,16 +13,19 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
 		gameBoard = createBoard();
-		System.out.println("Enter player Mark");
-		playerMark = playerMark();
-		computerMark = (playerMark == 'X') ? 'O' : 'X';
-		System.out.println("Player Mark: " + playerMark);
-		System.out.println("Computer mark: " + computerMark);
+		
+		toss();
+//		System.out.println("Enter player Mark");
+//		playerMark = playerMark();
+//		computerMark = (playerMark == 'X') ? 'O' : 'X';
+//		System.out.println("Player Mark: " + playerMark);
+//		System.out.println("Computer mark: " + computerMark);
 		showBoard(gameBoard);
 		int position;
 		System.out.println("Enter the pos for user move");
 		position = sc.nextInt();
 		userMove(position);
+		toss();
 	}
 
 	// UC1
@@ -46,31 +49,42 @@ public class TicTacToe {
 	public static void showBoard(char[] gameBoard) {
 		System.out.println("ShowBoard");
 		for (int i = 0; i < (gameBoard.length) / 3; i++) {
-			System.out.println("Line:" + (i + 1) + ": " + gameBoard[i * 3+1] + "|" + gameBoard[3 * i + 2] + "|"
+			System.out.println("Line:" + (i + 1) + ": " + gameBoard[i * 3 + 1] + "|" + gameBoard[3 * i + 2] + "|"
 					+ gameBoard[3 * i + 3]);
 		}
 	}
 
-	//UC4
-	public static int userMove(int position) {
-		
-		if(position <1 || position >9 || gameBoard[position]!=' ' 
-				|| gameBoard[position]!=computerMark)
-		{
+	// UC4 & UC5
+	public static void userMove(int position) {
+
+		if (position < 1 && position > 9 && gameBoard[position] != ' ') {
 			System.out.println("Invalid position, enter again ");
 			position = sc.nextInt();
 			userMove(position);
-		}
-		else
-		{
-			gameBoard[position]= playerMark;
+		} else {
+			gameBoard[position] = playerMark;
 			System.out.println("valid user position");
 			showBoard(gameBoard);
 		}
-		return position;
+//		return position;
 	}
-	
-	//UC5
-	
 
+	public static void toss() {
+		int random = (Math.random() <= 0.5) ? 1 : 2;
+		
+		if(random==1)
+		{
+			System.out.println("Computer Start");
+			computerMark = (Math.random() <= 0.5) ? 'X' : 'O';
+			playerMark =   (computerMark == 'X') ? 'O' : 'X';
+		}
+		else
+		{
+			System.out.println("Player Start");
+			playerMark =  playerMark();
+			computerMark = (playerMark == 'X') ? 'O' : 'X';
+		}
+		System.out.println("Player Mark: " + playerMark);
+		System.out.println("Computer mark: " + computerMark);
+	}
 }
