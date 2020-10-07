8c8a0ac087a044256cd378a8a752a7da2ab7fe38
nkneeraj <nkneerj641@gmail.com>
2020-10-07 09:41:52 +0530

    [Neeraj]Added UC2: Selection for player mark.

--- src/com/org/javaworkshop/TicTacToe.java
+++ src/com/org/javaworkshop/TicTacToe.java
@@ -1,11 +1,23 @@
 package com.org.javaworkshop;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
+	private static Scanner sc = new Scanner(System.in);
+	private static char playerMark;
+	private static char computerMark;
+	
 	//master
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
 		char[] gameBoard = createBoard();
+		
+		System.out.println("Enter player Mark");
+		playerMark = playerMark();
+		computerMark = (playerMark== 'X')? 'O':'X';
+		System.out.println("Player Mark: "+ playerMark);
+		System.out.println("Computer mark: "+ computerMark);
 	}
 
 	//UC1 
@@ -15,5 +27,13 @@ public class TicTacToe {
 			gameBoard[i] = ' ';
 		}
 		return gameBoard;
+	}
+	
+	public static char playerMark()
+	{
+		System.out.println("Choose player option from 'X' & 'O'");
+		String player = sc.next();
+		char playerMark = player.charAt(0);
+		return playerMark;
 	}
 }
